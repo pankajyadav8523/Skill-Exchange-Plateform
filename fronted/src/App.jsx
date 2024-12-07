3699553416d5d8c4956be9be576632ae62e1af05
pankajyadav8523 <py9621008569@gmail.com>
2024-12-07 11:35:06 +0530

    fixing conflict

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -9,31 +9,34 @@ import Navbar from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
 import Signup from "./components/SignUp"; // Ensure this matches the file name
 import Dashboard from "./components/dashboard/Dashboard";
+import { AuthProvider } from "../AuthContext";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Services />
-                <About />
-                <Testimonials />
-                <Footer />
-              </>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Services />
+                  <About />
+                  <Testimonials />
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
